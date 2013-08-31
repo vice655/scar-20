44e52ea6dd5ecf29b106b42ff5e4f0c8096adb1b
Vice Six Five Five <zeusmurphy@hotmail.com>
2013-08-31 03:26:48 +0100

    base

---
+++ SCAR-20 TTT/lua/weapon_ttt_scar20/shared.lua
@@ -0,0 +1,190 @@
+---- Example TTT custom weapon
+
+-- First some standard GMod stuff
+if SERVER then
+   AddCSLuaFile( "shared.lua" )
+end
+
+if CLIENT then
+   SWEP.PrintName = "SCAR-20"
+   SWEP.Slot      = 6 -- add 1 to get the slot number key
+
+   SWEP.ViewModelFOV  = 72
+   SWEP.ViewModelFlip = true
+end
+
+-- Always derive from weapon_tttbase.
+SWEP.Base				= "weapon_tttbase"
+
+--- Standard GMod values
+
+SWEP.HoldType			= "ar2"
+
+SWEP.Primary.Delay       = 0.20996
+SWEP.Primary.Recoil      = 1.537
+SWEP.Primary.Automatic   = false
+SWEP.Primary.Damage      = 34
+SWEP.Primary.Cone        = 0.003
+SWEP.Primary.Ammo        = "357"
+SWEP.Primary.ClipSize    = 20
+SWEP.Primary.ClipMax     = 40
+SWEP.Primary.DefaultClip = 40
+SWEP.Primary.Sound       = Sound( "scar20_unsil-1.wav" )
+SWEP.HeadshotMultiplier = 3
+
+SWEP.IronSightsPos = Vector( 6.05, -5, 2.4 )
+SWEP.IronSightsAng = Vector( 2.2, -0.1, 0 )
+
+SWEP.ViewModel  = "models/weapons/v_snip_g3sg1.mdl"
+SWEP.WorldModel = "models/weapons/w_snip_scar20.mdl"
+
+function SWEP:SetZoom(state)
+    if CLIENT then 
+       return
+    elseif IsValid(self.Owner) and self.Owner:IsPlayer() then
+       if state then
+          self.Owner:SetFOV(20, 0.3)
+       else
+          self.Owner:SetFOV(0, 0.2)
+       end
+    end
+end
+
+function SWEP:SecondaryAttack()
+    if not self.IronSightsPos then return end
+    if self.Weapon:GetNextSecondaryFire() > CurTime() then return end
+    
+    bIronsights = not self:GetIronsights()
+    
+    self:SetIronsights( bIronsights )
+    
+    if SERVER then
+        self:SetZoom(bIronsights)
+     else
+        self:EmitSound(self.Secondary.Sound)
+    end
+    
+    self.Weapon:SetNextSecondaryFire( CurTime() + 0.3)
+end
+
+function SWEP:Reload()
+    self.Weapon:DefaultReload( ACT_VM_RELOAD );
+    self:SetIronsights( false )
+    self:SetZoom(false)
+end
+
+
+function SWEP:Holster()
+    self:SetIronsights(false)
+    self:SetZoom(false)
+    return true
+end
+
+if CLIENT then
+   local scope = surface.GetTextureID("sprites/scope")
+   function SWEP:DrawHUD()
+      if self:GetIronsights() then
+         surface.SetDrawColor( 0, 0, 0, 255 )
+         
+         local x = ScrW() / 2.0
+         local y = ScrH() / 2.0
+         local scope_size = ScrH()
+
+         -- crosshair
+         local gap = 80
+         local length = scope_size
+         surface.DrawLine( x - length, y, x - gap, y )
+         surface.DrawLine( x + length, y, x + gap, y )
+         surface.DrawLine( x, y - length, x, y - gap )
+         surface.DrawLine( x, y + length, x, y + gap )
+
+         gap = 0
+         length = 50
+         surface.DrawLine( x - length, y, x - gap, y )
+         surface.DrawLine( x + length, y, x + gap, y )
+         surface.DrawLine( x, y - length, x, y - gap )
+         surface.DrawLine( x, y + length, x, y + gap )
+
+
+         -- cover edges
+         local sh = scope_size / 2
+         local w = (x - sh) + 2
+         surface.DrawRect(0, 0, w, scope_size)
+         surface.DrawRect(x + sh - 2, 0, w, scope_size)
+
+         surface.SetDrawColor(255, 0, 0, 255)
+         surface.DrawLine(x, y, x + 1, y + 1)
+
+         -- scope
+         surface.SetTexture(scope)
+         surface.SetDrawColor(255, 255, 255, 255)
+
+         surface.DrawTexturedRectRotated(x, y, scope_size, scope_size, 0)
+
+      else
+         return self.BaseClass.DrawHUD(self)
+      end
+   end
+
+   function SWEP:AdjustMouseSensitivity()
+      return (self:GetIronsights() and 0.2) or nil
+   end
+end
+
+
+--- TTT config values
+
+-- Kind specifies the category this weapon is in. Players can only carry one of
+-- each. Can be: WEAPON_... MELEE, PISTOL, HEAVY, NADE, CARRY, EQUIP1, EQUIP2 or ROLE.
+-- Matching SWEP.Slot values: 0      1       2     3      4      6       7        8
+SWEP.Kind = WEAPON_EQUIP1
+
+-- If AutoSpawnable is true and SWEP.Kind is not WEAPON_EQUIP1/2, then this gun can
+-- be spawned as a random weapon. Of course this AK is special equipment so it won't,
+-- but for the sake of example this is explicitly set to false anyway.
+SWEP.AutoSpawnable = false
+
+-- The AmmoEnt is the ammo entity that can be picked up when carrying this gun.
+SWEP.AmmoEnt = "item_ammo_357_ttt"
+
+-- CanBuy is a table of ROLE_* entries like ROLE_TRAITOR and ROLE_DETECTIVE. If
+-- a role is in this table, those players can buy this.
+SWEP.CanBuy = { ROLE_DETECTIVE }
+
+-- InLoadoutFor is a table of ROLE_* entries that specifies which roles should
+-- receive this weapon as soon as the round starts. In this case, none.
+SWEP.InLoadoutFor = nil
+
+-- If LimitedStock is true, you can only buy one per round.
+SWEP.LimitedStock = true
+
+-- If AllowDrop is false, players can't manually drop the gun with Q
+SWEP.AllowDrop = true
+
+-- If IsSilent is true, victims will not scream upon death.
+SWEP.IsSilent = false
+
+-- If NoSights is true, the weapon won't have ironsights
+SWEP.NoSights = false
+
+-- Equipment menu information is only needed on the client
+if CLIENT then
+   -- Path to the icon material
+   SWEP.Icon = "VGUI/ttt/snip_scar20.vtf"
+
+   -- Text shown in the equip menu
+   SWEP.EquipMenuData = {
+      type = "Weapon",
+      desc = "This is a prerelease version."
+   };
+end
+
+-- Tell the server that it should download our icon to clients.
+if SERVER then
+   -- It's important to give your icon a unique name. GMod does NOT check for
+   -- file differences, it only looks at the name. This means that if you have
+   -- an icon_ak47, and another server also has one, then players might see the
+   -- other server's dumb icon. Avoid this by using a unique name.
+   resource.AddFile("materials/VGUI/ttt/snip_emp_scar20.vtf")
+end
+
