9a41ea7bfb4e47ac00b1d3cbfc1a47a98facd5bd
vice655 <zeusmurphy@hotmail.com>
2013-09-02 17:26:13 +0100

    Update scar20resources.lua

--- SCAR-20 TTT/scar20resources.lua
+++ SCAR-20 TTT/scar20resources.lua
@@ -1,19 +1,11 @@
 -- put this file inside lua/autorun/server
 
-function AddDir(dir) 
-	local list = file.FindDir("../"..dir.."/*")
-	for _, fdir in pairs(list) do
-		if fdir != ".svn" then 
-			AddDir(dir.."/"..fdir)
-		end
-	end
- 
-	for k,v in pairs(file.Find(dir.."/*", true)) do
-		resource.AddFile(dir.."/"..v)
-	end
-end
- 
-AddDir("materials\models\weapons\v_models\go_scar")
+resource.AddFile("materials\models\weapons\v_models\go_scar\scope_normal.vtf")
+resource.AddFile("materials\models\weapons\v_models\go_scar\scope_scar.vtf")
+resource.AddFile("materials\models\weapons\v_models\go_scar\scope_scar.vmt")
+resource.AddFile("materials\models\weapons\v_models\go_scar\snip_scar20.vmt")
+resource.AddFile("materials\models\weapons\v_models\go_scar\snip_scar20.vtf")
+resource.AddFile("materials\models\weapons\v_models\go_scar\snip_scar20_exponent.vtf")
 resource.AddFile("models\weapons\v_snip_scar20.mdl")
 resource.AddFile("models\weapons\w_snip_scar20.mdl")
 resource.AddFile("sound\scar20_unsil-1.wav")
